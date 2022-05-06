474a81d483f9531845b0cfeb067ea530db925cb3
Oman Ramalho <omanapple42@hotmail.com>
2022-05-06 11:16:42 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./screens/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.tsx"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          300: '#996DFF',
+          500: '#8257e6',
+        }
+      },
+      borderRadius: {
+        md: '4px'
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar')
+  ],
+}
